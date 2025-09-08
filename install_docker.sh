c851ac4371ba26544c5d9764807c6654c9f25205
prudh <prudhhviraj2@gmail.com>
2025-09-08 18:58:24 +0530

    using the Kubectl

--- install_docker.sh
+++ install_docker.sh
@@ -19,4 +19,14 @@ sudo systemctl enable --now docker
 sudo usermod -aG docker ec2-user
 
 #kubectl installation
- 
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.3/2025-08-03/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+#eksctl installation
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin 
